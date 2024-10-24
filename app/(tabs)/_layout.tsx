060d5582156e346c63452e3f07ec5bf967f3325f
AndrewCrossan1 <andrew.crossan23@outlook.com>
2024-10-24 19:16:22 +0100

    Project Files added

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,74 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import Drawer from 'expo-router/drawer';
+import { DrawerContentComponentProps, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
+import { View, Image, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+function CustomDrawerContent(props: DrawerContentComponentProps) {
+    return (
+        <DrawerContentScrollView {...props}>
+            <View style={styles.profileContainer}>
+                {/* Profile Picture */}
+                <Image 
+                    source={{ uri: 'https://randomuser.me/api/portraits/men/41.jpg' }} 
+                    style={styles.profilePicture} 
+                />
+                <View style={styles.profileInfo}>
+                    <Text style={styles.profileName}>John Doe</Text>
+                    <Text style={styles.profileLink}>
+                        Senior Carer
+                    </Text>
+                </View>
+            </View>
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+            {/* Drawer Items */}
+            <DrawerItemList {...props} />
+        </DrawerContentScrollView>
+    );
 }
+
+export default function Layout() {
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Drawer
+                screenOptions={{ headerShown: true, drawerActiveTintColor: 'indigo', headerTintColor: 'indigo' }}
+                drawerContent={CustomDrawerContent}
+                >
+                <Drawer.Screen name="index" 
+                options={{
+                    drawerIcon: ({ color }) => <FontAwesome name="home" size={24} color={color} />,
+                    title: 'Home',
+                }}/>
+            </Drawer>
+        </GestureHandlerRootView>
+    )
+}
+
+// Styles
+const styles = StyleSheet.create({
+    profileContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 20
+    },
+    profilePicture: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        marginRight: 15,
+    },
+    profileInfo: {
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+    },
+    profileName: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    profileLink: {
+        color: 'indigo',
+        marginTop: 0,
+    },
+});
