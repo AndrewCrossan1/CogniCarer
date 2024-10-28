d84836d626a413dcc6a8ecd4ce529a59a1b29a7d
Andrew Crossan <andrew.crossan.2022@uni.strath.ac.uk>
2024-10-28 21:41:38 +0000

    Combined everything into one constant, added support for dark mode

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,71 +4,79 @@ import Drawer from 'expo-router/drawer';
 import { DrawerContentComponentProps, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import { View, Image, Text } from 'react-native';
 import { StyleSheet } from 'react-native';
-import { Link } from 'expo-router';
-
-function CustomDrawerContent(props: DrawerContentComponentProps) {
-    return (
-        <DrawerContentScrollView {...props}>
-            <View style={styles.profileContainer}>
-                {/* Profile Picture */}
-                <Image 
-                    source={{ uri: 'https://randomuser.me/api/portraits/men/41.jpg' }} 
-                    style={styles.profilePicture} 
-                />
-                <View style={styles.profileInfo}>
-                    <Text style={styles.profileName}>John Doe</Text>
-                    <Text style={styles.profileLink}>
-                        Senior Carer
-                    </Text>
-                </View>
-            </View>
-
-            {/* Drawer Items */}
-            <DrawerItemList {...props} />
-        </DrawerContentScrollView>
-    );
-}
+import {useThemeColor} from "@/hooks/useThemeColor";
 
 export default function Layout() {
+    const theme = useThemeColor();
+
+    const styles = StyleSheet.create({
+        profileContainer: {
+            flexDirection: 'row',
+            alignItems: 'center',
+            padding: 20
+        },
+        profilePicture: {
+            width: 50,
+            height: 50,
+            borderRadius: 25,
+            marginRight: 15,
+        },
+        profileInfo: {
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+        },
+        profileName: {
+            color: theme.text,
+            fontSize: 18,
+            fontWeight: 'bold',
+        },
+        profileLink: {
+            color: theme.text,
+            marginTop: 0,
+        },
+    });
+
+    function CustomDrawerContent(props: DrawerContentComponentProps) {
+        return (
+            <DrawerContentScrollView {...props}>
+                <View style={styles.profileContainer}>
+                    {/* Profile Picture */}
+                    <Image
+                        source={{ uri: 'https://randomuser.me/api/portraits/men/41.jpg' }}
+                        style={styles.profilePicture}
+                    />
+                    <View style={styles.profileInfo}>
+                        <Text style={styles.profileName}>John Doe</Text>
+                        <Text style={styles.profileLink}>
+                            Senior Carer
+                        </Text>
+                    </View>
+                </View>
+                {/* Separator */}
+                <View style={{ flex: 1, borderBottomWidth: 1, borderBottomColor: theme.drawerBottomBorderColor, marginBottom: 5, marginHorizontal: 12 }}/>
+                {/* Drawer Items */}
+                <DrawerItemList {...props} />
+            </DrawerContentScrollView>
+        );
+    }
+
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
             <Drawer
-                screenOptions={{ headerShown: true, drawerActiveTintColor: 'indigo', headerTintColor: 'indigo' }}
-                drawerContent={CustomDrawerContent}
-                >
-                <Drawer.Screen name="index" 
-                options={{
-                    drawerIcon: ({ color }) => <FontAwesome name="home" size={24} color={color} />,
-                    title: 'Home',
-                }}/>
+                screenOptions={{
+                    headerShown: true,
+                    drawerActiveTintColor: theme.primary,
+                    headerTintColor: theme.text,
+                    headerStyle: { backgroundColor: theme.light, shadowColor: theme.drawerBottomBorderColor },
+                    drawerStyle: { backgroundColor: theme.light },
+                }}
+                drawerContent={CustomDrawerContent}>
+                <Drawer.Screen name="index"
+                               options={{
+                                   drawerIcon: ({ color }) => <FontAwesome name="home" size={24} color={color} />,
+                                   title: 'Home',
+                               }}/>
             </Drawer>
         </GestureHandlerRootView>
     )
 }
-
-// Styles
-const styles = StyleSheet.create({
-    profileContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        padding: 20
-    },
-    profilePicture: {
-        width: 50,
-        height: 50,
-        borderRadius: 25,
-        marginRight: 15,
-    },
-    profileInfo: {
-        flexDirection: 'column',
-        alignItems: 'flex-start',
-    },
-    profileName: {
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    profileLink: {
-        color: 'indigo',
-        marginTop: 0,
-    },
-});
