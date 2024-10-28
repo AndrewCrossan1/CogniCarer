151fd8606930894caf5e287841ae35e4a54141ee
Andrew Crossan <andrew.crossan.2022@uni.strath.ac.uk>
2024-10-28 21:42:17 +0000

    Modified colours to current colour scheme

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,17 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    primary: '#419bf9',
+    dark: '#F5F5F5',
+    light: '#FFFFFF',
+    text: '#000000',
+    drawerBottomBorder: true,
+    drawerBottomBorderColor: '#E0E0E0',
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    primary: '#419bf9',
+    dark: '#121212',
+    light: '#1C1C1C',
+    text: '#FFFFFF',
+    drawerBottomBorderColor: '#333333',
   },
 };
