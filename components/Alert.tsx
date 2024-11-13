121f69429cc2df5c086eca3c0de6cb6ca9fd3cf3
AndrewCrossan1 <andrew.crossan23@outlook.com>
2024-11-13 20:02:06 +0000

    Created custom alert component

---
+++ components/Alert.tsx
@@ -0,0 +1,28 @@
+import {View, Text, TouchableOpacity} from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+export function Alert({ message, type, visible, onPress}: { message: string, type: "error" | "success", visible: boolean, onPress: () => void }) {
+    // Define colours
+    const alertColors =
+        type === "success" ? "bg-green-200 border-green-500 text-green-700" : "bg-red-200 border-red-500 text-red-700";
+
+    return (
+        <View
+            className={`transition ease-linear ${visible ? "visible" : "invisible" } absolute top-6 left-4 right-4 mx-auto border-1-4 rounded-lg ${alertColors} shadow  p-4`}
+            style={{ zIndex: 1000 }}>
+            <TouchableOpacity onPress={onPress} className={"absolute top-2 right-2"}>
+                <FontAwesome name={"close"} size={30} color={"#000"}/>
+            </TouchableOpacity>
+
+            {/* Alert Type */}
+            <View className={"flex flex-row items-center"}>
+                <FontAwesome name={type === "success" ? "check-circle" : "exclamation-circle"} size={20}/>
+                <Text className={"ml-2 font-bold"}>{type === "success" ? "Success" : "Error"}</Text>
+            </View>
+
+            {/* Alert Message */}
+            <Text className={"mt-2"}>{message}</Text>
+        </View>
+    );
+
+}
