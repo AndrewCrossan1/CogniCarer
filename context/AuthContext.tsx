b8be32537435732b045b5f63cfe19abd52d7b104
AndrewCrossan1 <andrew.crossan23@outlook.com>
2024-11-20 18:13:56 +0000

    Fixed login issue

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,9 +1,11 @@
 import { createContext, useContext, useState, ReactNode, FC } from "react";
 import {useLogout} from "@/hooks/useLogout";
 import {useLogin} from "@/hooks/useLogin";
+import {useGetUser} from "@/hooks/useGetUser";
 import {User} from "@/services/api/types";
 import {useUpdateUser} from "@/hooks/useUpdateUser";
 import {useRouter} from "expo-router";
+import * as SecureStore from "expo-secure-store";
 
 // Define the shape of the context
 interface AuthContextType {
@@ -12,6 +14,7 @@ interface AuthContextType {
     loginUser: (email: string, password: string) => Promise<boolean>;
     logoutUser: () => void;
     updateUser: (email: string, firstName: string, lastName: string) => Promise<boolean>
+    keyCheck: () => void;
 }
 
 // Create the context
@@ -26,6 +29,7 @@ export const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
     const { login } = useLogin();
     const { logout } = useLogout();
     const { update } = useUpdateUser();
+    const { get } = useGetUser();
     const router = useRouter();
 
     const loginUser = async (email: string, password: string): Promise<boolean> => {
@@ -40,16 +44,35 @@ export const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
     };
 
     const logoutUser = async () => {
+        // Call the logout hook
+        await logout();
+
         // Clear the token from the state
         setKey(null);
         setUser(null);
 
-        // Call the logout hook
-        await logout();
-
         // Redirect to the login page
         router.push("/(auth)/login");
     };
+
+    const keyCheck = async () => {
+        // Check if the key is still valid
+        if (!key) {
+            const key = await SecureStore.getItemAsync("token");
+            if (key) {
+                setKey(key);
+                // Get the user data
+                const user = await get();
+                if (user) {
+                    setUser(user);
+                    // Push to home page
+                    router.push("/(app)");
+                }
+            } else {
+                router.push("/(auth)/login");
+            }
+        }
+    }
 
     const updateUser = async (email: string, firstName: string, lastName: string): Promise<boolean> => {
         const updatedUser = await update(email, firstName, lastName);
@@ -61,7 +84,7 @@ export const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
     }
 
     return (
-        <AuthContext.Provider value={{ key, user, loginUser, logoutUser, updateUser }}>
+        <AuthContext.Provider value={{ key, user, loginUser, logoutUser, updateUser, keyCheck }}>
             {children}
         </AuthContext.Provider>
     );
