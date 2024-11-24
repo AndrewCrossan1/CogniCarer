4d2bec7397cdec7b171ab9c2a9cd7c95f0292748
AndrewCrossan1 <andrew.crossan23@outlook.com>
2024-11-24 18:16:49 +0000

    Display card layout

--- app/(app)/(storyboard)/(tabs)/templateList.tsx
+++ app/(app)/(storyboard)/(tabs)/templateList.tsx
@@ -1,26 +1,31 @@
-import {ActivityIndicator, StyleSheet, Text, View} from "react-native";
+import {ActivityIndicator, ScrollView, StyleSheet, Text, View} from "react-native";
 import {SearchInput} from "@/components/SearchInput";
 import {useGetTemplates} from "@/hooks/storyboard/useGetTemplates";
 import {useThemeColor} from "@/hooks/useThemeColor";
 import {useEffect, useState} from "react";
 import {Template} from "@/services/api/types";
+import {TemplateCard} from "@/components/Storyboard/TemplateCard";
 
 export default function TemplateList() {
     const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-        }
+        container: {}
     });
 
-    const {getTemplates, getTemplate, templates, loading} = useGetTemplates();
+    const {getTemplates, templates, loading} = useGetTemplates();
     const theme = useThemeColor();
     const [Templates, setTemplates] = useState([] as Template[]);
+    const [mounted, setMounted] = useState(false);
 
     useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    useEffect(() => {
+        if (!mounted) return;
         getTemplates().then(() => {
             setTemplates(templates);
         });
-    }, []);
+    }, [mounted]);
 
     const handleSearch = (s: string) => {
         // Filter the templates based on the search string
@@ -31,13 +36,17 @@ export default function TemplateList() {
     }
 
     return (
-        <View style={styles.container} className={"dark:bg-neutral-800 p-4"}>
+        <View style={{flex: 1}} className={"dark:bg-neutral-800 p-4"}>
             <SearchInput placeholder={"Search for templates..."} onSearch={handleSearch}/>
-            {loading ? (<ActivityIndicator size={"large"} color={theme.primary}/>)
-                : Templates.map((template: Template) => {
-                    return <Text key={template.uuid} className={"text-white"}>{template.name}</Text>
-                })
-            }
+            <ScrollView style={{flex: 1}}>
+                <View style={styles.container} className={"p-1"}>
+                    {loading ? (<ActivityIndicator size={"large"} color={theme.primary}/>)
+                        : Templates.map((template: Template) => {
+                            return <TemplateCard key={template.uuid} template={template}/>
+                        })
+                    }
+                </View>
+            </ScrollView>
         </View>
     )
 }
