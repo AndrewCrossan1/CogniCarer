a4f20845216d05fdbe5fa80311f6ff430bf91d21
Andrew Crossan <andrew.crossan.2022@uni.strath.ac.uk>
2024-11-25 00:18:14 +0000

    Removed modal from searchinput and added to parent

--- app/(app)/(storyboard)/(tabs)/templateList.tsx
+++ app/(app)/(storyboard)/(tabs)/templateList.tsx
@@ -1,24 +1,44 @@
-import {ActivityIndicator, ScrollView, StyleSheet, Text, View} from "react-native";
+import {ActivityIndicator, Modal, ScrollView, StyleSheet, Text, View} from "react-native";
 import {SearchInput} from "@/components/SearchInput";
 import {useGetTemplates} from "@/hooks/storyboard/useGetTemplates";
 import {useThemeColor} from "@/hooks/useThemeColor";
 import {useEffect, useState} from "react";
 import {Template} from "@/services/api/types";
 import {TemplateCard} from "@/components/Storyboard/TemplateCard";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {BlurView} from "expo-blur";
 
 export default function TemplateList() {
     const styles = StyleSheet.create({
-        container: {}
+        container: {},
+        modalView: {
+            shadowColor: '#000',
+            shadowOffset: {
+                width: 0,
+                height: 2,
+            },
+            shadowOpacity: 0.5,
+            shadowRadius: 4,
+            elevation: 5,
+        },
     });
 
     const {getTemplates, templates, loading} = useGetTemplates();
     const theme = useThemeColor();
     const [Templates, setTemplates] = useState([] as Template[]);
     const [mounted, setMounted] = useState(false);
+    const [filtered, setFiltered] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
 
     useEffect(() => {
         setMounted(true);
     }, []);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setModalVisible(false);
+        }, 4000);
+        return () => clearTimeout(timer);
+    }, [modalVisible]);
 
     useEffect(() => {
         if (!mounted) return;
@@ -32,17 +52,47 @@ export default function TemplateList() {
         const filteredTemplates = templates.filter((template: Template) => {
             return template.name.toLowerCase().includes(s.toLowerCase());
         });
+        setFiltered(true);
         setTemplates(filteredTemplates);
     }
 
     return (
         <View style={{flex: 1}} className={"dark:bg-neutral-800 p-4"}>
-            <SearchInput placeholder={"Search for templates..."} onSearch={handleSearch}/>
+            <SearchInput placeholder={"Search for templates..."} onSearch={handleSearch} modalVisible={modalVisible} modalVisibleFun={() => setModalVisible(!modalVisible)}/>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}>
+                <BlurView intensity={100} style={[StyleSheet.absoluteFill, styles.modalView]}/>
+                <View className={"mt-safe mx-safe-or-4 dark:bg-neutral-900 rounded-lg elevation-md p-4"}>
+                    <View className={"flex-row justify-start items-center"}>
+                        <FontAwesome style={{marginRight: 10}} name={"info-circle"} size={30} color={"white"}/>
+                        <View className={"flex-1 flex-row items-center"}>
+                            <Text className={"text-lg dark:text-white font-bold w-full"}>Search Information</Text>
+                        </View>
+                        <FontAwesome name={"close"} size={30} color={"red"}
+                                     onPress={() => setModalVisible(!modalVisible)}/>
+                    </View>
+                    <View style={{flex: 1, borderBottomWidth: 1, borderBottomColor: "white", marginVertical: 5}}/>
+                    <Text className={"dark:text-gray-400 mt-2"}>
+                        Use the search bar to search for a specific item.
+                    </Text>
+                    <Text className={"dark:text-gray-400 my-2"}>
+                        You can search by name, ID, or any other relevant information.
+                    </Text>
+                </View>
+
+            </Modal>
             <ScrollView style={{flex: 1}}>
                 <View style={styles.container} className={"p-1"}>
-                    {loading ? <ActivityIndicator size={"large"} color={theme.primary}/> : templates.map((template: Template) => {
+                    {loading && <ActivityIndicator color={theme.primary} size={"large"}/>}
+                    {!loading && Templates.map((template: Template) => {
                         return <TemplateCard key={template.uuid} template={template}/>
                     })}
+                    {filtered && Templates.length === 0 && <Text className={"text-center text-lg text-neutral-500"}>No templates found</Text>}
                 </View>
             </ScrollView>
         </View>
--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,21 +1,13 @@
 import {View, TextInput, StyleSheet, TouchableOpacity, Modal, Text, SafeAreaView} from "react-native";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import {useThemeColor} from "@/hooks/useThemeColor";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
-export function SearchInput({placeholder, onSearch}: { placeholder: string, onSearch: (s: string) => void }) {
+export function SearchInput({placeholder, onSearch, modalVisibleFun}: { placeholder: string, onSearch: (s: string) => void, modalVisible: boolean, modalVisibleFun: () => void }) {
     const theme = useThemeColor();
     const [borderColor, setBorderColor] = useState("border-neutral-900");
     const [iconColor, setIconColor] = useState("dimgrey");
-    const [modalVisible, setModalVisible] = useState(false);
     const [searchValue, setSearchValue] = useState("")
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setModalVisible(false);
-        }, 5000);
-        return () => clearTimeout(timer);
-    }, [modalVisible]);
 
     const onChangeText = (s: string) => {
         setSearchValue(s);
@@ -36,15 +28,6 @@ export function SearchInput({placeholder, onSearch}: { placeholder: string, onSe
         input: {
             flex: 1,
             color: theme.text,
-        },
-        modalView: {
-            shadowColor: '#000',
-            shadowOffset: {
-                width: 0,
-                height: 2,
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 4,
         },
     })
 
@@ -78,35 +61,10 @@ export function SearchInput({placeholder, onSearch}: { placeholder: string, onSe
                     />
                 </View>
                 <TouchableOpacity onPress={() => {
-                    setModalVisible(true)
+                    modalVisibleFun();
                 }} className={"rounded-lg"}>
                     <FontAwesome style={{marginLeft: 10}} name={"info-circle"} size={20} color={iconColor}/>
                 </TouchableOpacity>
-                <Modal
-                    animationType="slide"
-                    transparent={true}
-                    visible={modalVisible}
-                    onRequestClose={() => {
-                        setModalVisible(!modalVisible);
-                    }}>
-                    <View className={"mt-safe mx-safe-or-4 dark:bg-neutral-900 rounded-lg elevation-md p-4"}>
-                        <View className={"flex-row justify-start items-center"}>
-                            <FontAwesome style={{marginRight: 10}} name={"info-circle"} size={30} color={"white"}/>
-                            <View className={"flex-1 flex-row items-center"}>
-                                <Text className={"text-lg dark:text-white font-bold w-full"}>Search Information</Text>
-                            </View>
-                            <FontAwesome name={"close"} size={30} color={"red"}
-                                         onPress={() => setModalVisible(!modalVisible)}/>
-                        </View>
-                        <View style={{flex: 1, borderBottomWidth: 1, borderBottomColor: "white", marginVertical: 5}}/>
-                        <Text className={"dark:text-gray-400 mt-2"}>
-                            Use the search bar to search for a specific item.
-                        </Text>
-                        <Text className={"dark:text-gray-400 my-2"}>
-                            You can search by name, ID, or any other relevant information.
-                        </Text>
-                    </View>
-                </Modal>
             </View>
         </SafeAreaView>
     )
