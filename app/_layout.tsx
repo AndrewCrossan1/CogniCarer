ef36ede9edd2a35f941b1b338c4a22133202034c
AndrewCrossan1 <andrew.crossan23@outlook.com>
2024-12-11 02:23:43 +0000

    Added redux provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,31 +3,40 @@ import "../global.css";
 import { AuthProvider } from "@/context/AuthContext";
 import AuthHandler from "@/app/AuthHandler";
 import { configureReanimatedLogger, ReanimatedLogLevel} from "react-native-reanimated";
+import {Provider} from "react-redux";
+import {store} from "@/services/store/store";
+import {BackHandler} from "react-native";
 
 configureReanimatedLogger({
     level: ReanimatedLogLevel.warn,
     strict: false,
 })
 
+BackHandler.addEventListener("hardwareBackPress", () => {
+    return true;
+});
+
 export default function RootLayout() {
     return (
+        <Provider store={store}>
         <AuthProvider>
             <AuthHandler/>
-            <Stack
-                screenOptions={{
-                    headerShown: false
-                }}>
-                <Stack.Screen name="(app)"
-                              options={{
-                                  gestureEnabled: false
-                              }}
-                />
-                <Stack.Screen name="(auth)/login"
-                              options={{
-                                  gestureEnabled: false
-                              }}
-                />
-            </Stack>
+                <Stack
+                    screenOptions={{
+                        headerShown: false
+                    }}>
+                    <Stack.Screen name="(app)"
+                                  options={{
+                                      gestureEnabled: false,
+                                  }}
+                    />
+                    <Stack.Screen name="(auth)/login"
+                                  options={{
+                                      gestureEnabled: false
+                                  }}
+                    />
+                </Stack>
         </AuthProvider>
+        </Provider>
     )
 }
