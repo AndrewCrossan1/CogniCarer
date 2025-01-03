69ce15f4f8f3390c3c42bdfb61908dd97af1f515
AndrewCrossan1 <andrew.crossan23@outlook.com>
2025-01-03 15:52:53 +0000

    Created (auth) root layout

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,18 @@
+import {Image, View} from "react-native";
+import React from "react";
+import {Stack} from "expo-router";
+
+export default function Layout() {
+  return (
+    <View className={"flex-1 dark:bg-neutral-900 bg-neutral-100"}>
+        <Image source={require("@/assets/images/layered-waves-haikei.png")} className={"xs:h-8 sm:h-16 md:h-32 lg:h-40"} />
+        <Image source={require("@/assets/images/logo.png")} className={"mx-auto xs:mt-2 sm:mt-4 md:mt-6 lg:mt-8"}/>
+        <Stack
+            screenOptions={{
+                headerShown: false,
+                animation: "simple_push"
+            }}
+        />
+    </View>
+  );
+}
