36b6a0af89927676e6de44ff2868a5171a0dd4b2
AndrewCrossan1 <andrew.crossan23@outlook.com>
2025-01-27 23:03:28 +0000

    Implemented full retrieval and selection

--- app/(app)/(reminisce)/(tabs)/Pictures.tsx
+++ app/(app)/(reminisce)/(tabs)/Pictures.tsx
@@ -1,20 +1,50 @@
-import {Image, ScrollView, Text, TouchableOpacity, useColorScheme, View} from "react-native";
+import {
+    Image,
+    Modal,
+    RefreshControl,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    useColorScheme,
+    View
+} from "react-native";
 import {MaterialIcons} from "@expo/vector-icons";
-import {useThemeColor} from "@/hooks/useThemeColor";
-import {useAppSelector} from "@/hooks/store/hooks";
 import colors from "tailwindcss/colors";
 import {Alert} from "@/components/Alert";
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
+import Animated, {Easing, useAnimatedStyle, useSharedValue, withTiming} from "react-native-reanimated";
+import * as Haptics from "expo-haptics";
+import {Picture, UserAlbum} from "@/services/api/types";
+import {useReminisce} from "@/hooks/useReminisce";
+import {BlurView} from "expo-blur";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 const Pictures = () => {
 
     const image = require('@/assets/images/undraw_asset-selection_jrie.png');
-    const theme = useThemeColor();
-    const user = useAppSelector(state => state.user.user);
     const mode = useColorScheme();
 
     const [visible, setVisible] = useState(false);
-    const [selected, setSelected] = useState([]);
+    const [selected, setSelected] = useState<Picture[]>([] as Picture[]);
+    const [pictures, setPictures] = useState<Picture[]>([] as Picture[]);
+    const [message, setMessage] = useState("");
+    const [refreshing, setRefreshing] = useState(false);
+    const [canSelect, setCanSelect] = useState(false);
+    const [confirmVisible, setConfirmVisible] = useState(false);
+    const [alertType, setAlertType] = useState<"success" | "error">("error");
+
+    const onSelectPress = () => {
+        if (!canSelect) {
+            setCanSelect(true);
+        } else {
+            setCanSelect(false);
+            setSelected([]);
+        }
+        console.debug("Selected Pictures: ", selected.map(picture => picture.title));
+    }
+
+    const {getPictures, error, loading, getAlbumName} = useReminisce()
 
     // Timer to automatically close the alert after 3 seconds
     useEffect(() => {
@@ -25,11 +55,68 @@ const Pictures = () => {
         }
     }, [visible]);
 
+    // Fetch pictures on component mount
+    useEffect(() => {
+        const fetchPictures = async () => {
+            const fetchedPictures = await getPictures();
+            if (fetchedPictures) {
+                setPictures(fetchedPictures);
+                console.debug("Pictures fetched, found", fetchedPictures.length);
+            } else {
+                setMessage("No Pictures found");
+                setVisible(true);
+            }
+        }
+
+        fetchPictures();
+    }, []);
+
+
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+        getPictures().then((valid) => {
+            if (!valid) return;
+            setRefreshing(false);
+            console.debug("Pictures refreshed, found: ", valid.length);
+            setPictures(valid);
+        });
+    }, [getPictures]);
+
+    // Selected Animations
+    const maxHeight = useSharedValue(0)
+
+    useEffect(() => {
+        if (selected.length > 0) {
+            maxHeight.value = withTiming(200, {
+                duration: 1500,
+                easing: Easing.out(Easing.exp)
+            })
+        } else {
+            maxHeight.value = withTiming(0, {
+                duration: 1500,
+                easing: Easing.out(Easing.exp)
+            })
+        }
+    }, [selected.length]);
+
+    const animatedStyle = useAnimatedStyle(() => ({
+        maxHeight: maxHeight.value,
+        opacity: maxHeight.value > 0 ? 1 : 0
+    }))
+
     return (
-        <ScrollView style={{backgroundColor: mode === 'dark' ? colors.neutral[800] : colors.white}} contentContainerStyle={{flexGrow: 1}}>
-            <View className={"flex-1 items-center dark:bg-neutral-800 pb-10"}>
+        <ScrollView style={{backgroundColor: mode === 'dark' ? colors.neutral[800] : colors.white}} contentContainerStyle={{flexGrow: 1}}
+                    refreshControl={
+                        <View>
+                            <RefreshControl title={"Refreshing..."} titleColor={colors.neutral[400]}
+                                            tintColor={colors.neutral[400]} refreshing={refreshing}
+                                            onRefresh={onRefresh}/>
+                        </View>
+                    }>
+            <View className={"flex-1 items-center dark:bg-neutral-800 pb-4"}>
                 <View className={"w-full bg-blue-500 dark:bg-neutral-800 p-6"}>
-                    <Alert message={"No pictures selected!"} type={"error"} onPress={() => {
+                    <Alert message={message} type={alertType} onPress={() => {
                         setVisible(false);
                     }} visible={visible} />
                     <View className={"flex-row items-center"}>
@@ -47,38 +134,182 @@ const Pictures = () => {
                             </Text>
                         </View>
                     </View>
-                    <View className={"flex-row justify-between items-center w-full"}>
+                    <View className={"flex-row items-center justify-center gap-5 w-full"}>
                         <TouchableOpacity
-                            className="flex-row items-center mt-3 rounded-lg p-2 bg-blue-600 dark:bg-neutral-900 drop-shadow-md shadow-blue-500/50">
+                            className="flex-row items-center mt-3 w-1/2 rounded-lg p-2 bg-blue-600 dark:bg-neutral-900 drop-shadow-md shadow-blue-500/50">
                             <MaterialIcons name="upload" size={24} color="white" className={"mr-1"} />
                             <Text className="text-white">
-                                Upload
+                                Add Pictures
                             </Text>
                         </TouchableOpacity>
                         <TouchableOpacity
-                            className="flex-row items-center mt-3 rounded-lg p-2 bg-red-500 drop-shadow-md shadow-blue-500/50"
                             onPress={() => {
-                                if (selected.length === 0) {
-                                    setVisible(true);
-                                }
-                            }}>
-                            <MaterialIcons name="delete" size={24} color="white" className={"mr-1"} />
+                                onSelectPress();
+                            }}
+                            className={`flex-row items-center mt-3 w-1/2 rounded-lg p-2 ${canSelect ? 'bg-amber-500' : 'bg-blue-600'} dark:bg-neutral-900 drop-shadow-md shadow-blue-500/50`}>
+                            <MaterialIcons name={canSelect ? 'cancel' : 'edit'} size={24} color="white" className={"mr-1"} />
                             <Text className="text-white">
-                                Delete Picture
-                            </Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            className="flex-row items-center mt-3 rounded-lg p-2 bg-amber-500 dark:bg-amber-500 drop-shadow-md shadow-blue-500/50">
-                            <MaterialIcons name="edit" size={24} color="white" className={"mr-1"} />
-                            <Text className="text-white">
-                                Edit Picture
+                                {canSelect ? "Cancel" : "Select Pictures"}
                             </Text>
                         </TouchableOpacity>
                     </View>
                 </View>
+                {/* Selected album dropdown choices (Delete, etc) */}
+                <Animated.View style={[styles.selectedContainer, animatedStyle]}>
+                    {canSelect && selected.length > 0 &&
+                      <View className={"w-full px-4 pt-4 flex-row items-center justify-between"}>
+                        <Text className={"dark:text-white text-lg font-bold"}>
+                          Selected Albums
+                        </Text>
+                        <Text className={"text-blue-500 text-sm"}>
+                            {selected.length} Selected
+                        </Text>
+                      </View>
+                    }
+                    {canSelect && selected.length > 0 &&
+                      <View>
+                        <View className={"flex-row items-center justify-center gap-5 px-4 w-full"}>
+                          <TouchableOpacity
+                            onPress={() => {
+                                setConfirmVisible(!confirmVisible);
+
+                            }}
+                            className="flex-row items-center mt-3 w-full rounded-lg p-2 bg-red-600 dark:bg-neutral-900 drop-shadow-md shadow-blue-500/50">
+                            <MaterialIcons name="delete" size={24} color="white" className={"mr-1"} />
+                            <Text className="text-white">
+                              Delete
+                            </Text>
+                          </TouchableOpacity>
+                        </View>
+                      </View>
+                    }
+                </Animated.View>
+
+                {/* Every 2 albums (Columns) create a new row */}
+                {!loading &&
+                  <View className={"w-full p-4"}>
+                    <View className={"flex-row flex-wrap justify-start"}>
+                        {pictures.map((picture, index) => (
+                            <TouchableOpacity
+                                key={picture.uuid}
+                                activeOpacity={0.8}
+                                onPress={() => {
+                                    if (!canSelect) return;
+                                    if (selected.includes(picture)) {
+                                        setSelected(selected.filter(selectedPicture => selectedPicture !== picture));
+                                    } else {
+                                        setSelected([...selected, picture]);
+                                    }
+                                }}
+                                className={"w-1/2 p-2"}>
+                                <View className={"w-full relative"}>
+                                    {canSelect && <View
+                                      className={`absolute top-2 right-2 w-6 h-6 rounded-full border-1 border-gray-300 justify-center items-center ${
+                                          selected.includes(picture) ? "bg-blue-500 border-blue-500" : ""
+                                      }`}
+                                    >
+                                        {selected.includes(picture) ? (
+                                                <MaterialIcons name={"check"} size={20} color={"white"} />
+                                            ) :
+                                            <MaterialIcons name={"add"} size={20} color={"blue"} />
+                                        }
+                                    </View>
+                                    }
+                                </View>
+                                <View className={`w-full ${canSelect && selected.includes(picture) ? "opacity-50" : null}`}>
+                                    <View className={"justify-center items-center"}>
+                                        {/* Image */}
+                                        <Image
+                                            className={"rounded-t-lg border-t-2 border-l-2 border-r-2 border-gray-100"}
+                                            source={{uri: picture.image_url}}
+                                            style={{width: "100%", height: 175}}
+                                        />
+                                    </View>
+                                    <View className={"rounded-b-lg bg-neutral-100 dark:bg-neutral-900 p-4"}>
+                                        <View className={"flex-row items-center justify-between"}>
+                                            <Text className={"dark:text-white text-lg font-bold"}>
+                                                {picture.title}
+                                            </Text>
+                                        </View>
+                                        <Text className={"text-blue-500 text-sm"}>
+                                            {picture.albumActual ? picture.albumActual.title : "No Album"}
+                                        </Text>
+                                        <Text className={"text-xs text-neutral-500"}>
+                                            Uploaded: {new Date(picture.created_at).toDateString()}
+                                        </Text>
+                                    </View>
+                                </View>
+                            </TouchableOpacity>
+                        ))}
+                    </View>
+                  </View>
+                }
             </View>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={confirmVisible}
+                onRequestClose={() => {
+                    setConfirmVisible(!confirmVisible);
+                }}>
+                <BlurView intensity={75} style={[StyleSheet.absoluteFill, styles.modalView]}/>
+                <View className={"mt-safe mx-safe-or-4 dark:bg-neutral-900 bg-white rounded-lg elevation-md p-4"}>
+                    <View className={"flex-row justify-start items-center"}>
+                        <FontAwesome name={"close"} size={30} color={"red"}
+                                     onPress={() => setConfirmVisible(!confirmVisible)}/>
+                        <Text className={"text-lg ml-4 dark:text-white font-bold w-full"}>Delete Albums</Text>
+                    </View>
+                    <View style={{flex: 1, borderBottomWidth: 1, borderBottomColor: "white", marginVertical: 5}}/>
+                    <Text className={"dark:text-gray-400 mt-2"}>
+                        Are you sure you want to delete the selected albums?
+                    </Text>
+                    {selected.map((album, index) => (
+                        <View className={"mt-2 p-4"}>
+                            <Text className={"dark:text-gray-400 mt-2"}>
+                                {index + 1}. {album.title}
+                            </Text>
+                        </View>
+                    ))}
+                    <View className={"flex flex-row justify-center gap-2"}>
+                        <TouchableOpacity
+                            className={"flex-row items-center w-1/2 mt-3 rounded-lg p-2 bg-red-600 dark:bg-neutral-900 drop-shadow-md shadow-blue-500/50"}>
+                            <MaterialIcons name="delete" size={24} color="white" className={"mr-1"} />
+                            <Text className="text-white">
+                                Delete
+                            </Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            onPress={() => setConfirmVisible(!confirmVisible)}
+                            className={"flex-row items-center w-1/2 mt-3 rounded-lg p-2 bg-blue-600 dark:bg-neutral-900 drop-shadow-md shadow-blue-500/50"}>
+                            <MaterialIcons name="cancel" size={24} color="white" className={"mr-1"} />
+                            <Text className="text-white">
+                                Cancel
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
+
         </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    selectedContainer: {
+        flex: 1,
+        overflow: 'hidden',
+    },
+    modalView: {
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+})
 
 export default Pictures;
