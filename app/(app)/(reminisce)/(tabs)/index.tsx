fe8bd2a88beb5025f7ef6a31d401b7a5520d9eac
Andrew Crossan <andrew.crossan.2022@uni.strath.ac.uk>
2025-01-29 00:00:03 +0000

    Displayed albums and added refresh control

--- app/(app)/(reminisce)/(tabs)/index.tsx
+++ app/(app)/(reminisce)/(tabs)/index.tsx
@@ -11,6 +11,10 @@ import {Link} from "expo-router";
 import {Calendar} from "react-native-calendars";
 import colors from "tailwindcss/colors";
 import {useThemeColor} from "@/hooks/useThemeColor";
+import {useReminisce} from "@/hooks/useReminisce";
+import {useEffect, useState} from "react";
+import {UserAlbum} from "@/services/api/types";
+import {Alert} from "@/components/Alert";
 
 // TODO: Retrieve most populated albums, most recent entry and most recent entries.
 
@@ -20,11 +24,40 @@ const index = () => {
     const quote = useAppSelector(state => state.quote.quote);
     const theme = useThemeColor();
     const mode = useColorScheme();
+    const [albums, setAlbums] = useState([] as UserAlbum[]);
+    const [alertType, setAlertType] = useState<"success" | "error">("error");
+    const [message, setMessage] = useState("");
+    const [visible, setVisible] = useState(false);
+
+    const {getAlbums, loading } = useReminisce()
+    const albumCover = require('@/assets/images/yes.png');
+
+    useEffect(() => {
+        const fetchAlbums = async () => {
+            const fetchedAlbums = await getAlbums();
+            if (fetchedAlbums) {
+                setAlbums(fetchedAlbums.sort((a, b) => {
+                    return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
+                }).slice(0, 3));
+                console.debug("Albums fetched and sorted, found: ", fetchedAlbums.length);
+            } else {
+                setMessage("No albums found");
+                setVisible(true);
+            }
+        }
+
+        fetchAlbums();
+    }, []);
+
+    // Sort by most recently created and slice to
 
     return (
         <ScrollView style={{backgroundColor: mode === 'dark' ? colors.neutral[800] : colors.white}} contentContainerStyle={{flexGrow: 1}}>
             <View className={"flex-1 items-center dark:bg-neutral-800 pb-10"}>
                 <View className={"w-full bg-blue-500 dark:bg-neutral-800 p-6"}>
+                    <Alert message={message} type={alertType} onPress={() => {
+                        setVisible(false);
+                    }} visible={visible} />
                     <View className={"flex-row items-center"}>
                         <Image
                             source={{uri: user?.profile_image }}
@@ -94,10 +127,43 @@ const index = () => {
                         </View>
                     </View>
                     <View className={"flex-row gap-4 justify-between w-full mt-4"} key={"albums"}>
-                        <View className={"flex-1 border bg-neutral-100 dark:bg-neutral-900 dark:border-neutral-900 dark:shadow-sm border-neutral-300 rounded-lg p-4"}>
-                            <Text className={"dark:text-white"}>
+                        <View className={"flex-1 border bg-neutral-100 dark:bg-neutral-900 dark:border-neutral-900 dark:shadow-sm border-neutral-300 rounded-lg pt-4 px-4 pb-2"}>
+                            <Text className={"dark:text-white font-bold"}>
                                 Albums
                             </Text>
+                            {!loading &&
+                                <View className={"w-full mt-1"}>
+                                    <View className={"flex-row flex-wrap justify-start"}>
+                                        {albums.map((album, index) => (
+                                            <TouchableOpacity
+                                                key={album.uuid}
+                                                activeOpacity={0.8}
+                                                className={"w-1/3 p-2"}>
+                                                <View className={`w-full`}>
+                                                    <View className={"justify-center items-center"}>
+                                                        {/* Album Cover */}
+                                                        <Image
+                                                            className={"rounded-t-lg"}
+                                                            source={albumCover}
+                                                            style={{width: "100%", height: 75}}
+                                                        />
+                                                    </View>
+                                                    <View className={"rounded-b-lg bg-neutral-100 dark:bg-black p-4"}>
+                                                        <View className={"flex-row items-center justify-between"}>
+                                                            <Text className={"dark:text-white text-lg font-bold"}>
+                                                                {album.title}
+                                                            </Text>
+                                                        </View>
+                                                        <Text className={"text-sm text-blue-500"}>
+                                                            {album.patientActual?.first_name} {album.patientActual?.last_name}
+                                                        </Text>
+                                                    </View>
+                                                </View>
+                                            </TouchableOpacity>
+                                        ))}
+                                    </View>
+                                </View>
+                            }
                         </View>
                     </View>
                     <View className={"flex-row gap-4 justify-between w-full mt-4"} key={"dates"}>
