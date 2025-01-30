df081c538a478b9364e033e1a2818d31d579c0ac
AndrewCrossan1 <andrew.crossan23@outlook.com>
2025-01-30 17:50:24 +0000

    Created NewAlbumForm

--- components/reminisce/NewAlbumForm.tsx
+++ components/reminisce/NewAlbumForm.tsx
@@ -1,26 +1,40 @@
-import {Modal, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
+import {Modal, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {BlurView} from "expo-blur";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {Dropdown} from "@/components/reminisce/Dropdown";
-import {useEffect, useState} from "react";
+import {Dropdown} from "@/components/forms/Dropdown";
+import {useEffect, useRef, useState} from "react";
 import {Patient} from "@/services/api/types";
 import {usePatients} from "@/hooks/patients/usePatients";
 import {useAppSelector} from "@/hooks/store/hooks";
 import {MaterialIcons} from "@expo/vector-icons";
 import {useReminisce} from "@/hooks/useReminisce";
+import InputGroup, {InputGroupRef} from "@/components/forms/InputGroup";
 
+/**
+ * NewAlbumProps interface
+ * @interface NewAlbumProps
+ * @property {() => void} onSubmitted
+ * @property {boolean} visible
+ * @property {() => void} onClose
+ */
 interface NewAlbumProps {
     onSubmitted: () => void;
     visible: boolean;
     onClose: () => void;
 }
 
-export const NewAlbum = (props: NewAlbumProps) => {
+/**
+ * NewAlbum component
+ * @desc A modal component which can be used to create a new album
+ * @param {NewAlbumProps} props
+ * @constructor
+ */
+const NewAlbum = (props: NewAlbumProps) => {
     // Hooks
     const {getPatients} = usePatients();
     const {visible, onSubmitted} = props;
     const user = useAppSelector(state => state.user.user);
-    const { newAlbum } = useReminisce();
+    const {newAlbum} = useReminisce();
 
     // States for the new album
     const [albumName, setAlbumName] = useState("");
@@ -29,13 +43,15 @@ export const NewAlbum = (props: NewAlbumProps) => {
     const [patients, setPatients] = useState([] as { value: string, display: string }[]);
 
     // Error states
-    const [albumNameError, setAlbumNameError] = useState("");
-    const [descriptionError, setDescriptionError] = useState("");
-    const [patientNameError, setPatientNameError] = useState("");
+    const [albumNameError, setAlbumNameError] = useState(false);
+    const [descriptionError, setDescriptionError] = useState(false);
+
+    // Refs for input fields
+    const albumNameRef = useRef<InputGroupRef>(null);
+    const descriptionRef = useRef<InputGroupRef>(null);
 
     // Fetch patients
     useEffect(() => {
-
         const fetchPatients = async () => {
             const fetchedPatients = await getPatients();
             if (fetchedPatients) {
@@ -46,62 +62,67 @@ export const NewAlbum = (props: NewAlbumProps) => {
                     }
                 });
                 setPatients(patients);
-                console.debug("Patients fetched, found: ", fetchedPatients.length);
+                console.debug("[NewAlbumForm] Patients fetched, found: ", fetchedPatients.length);
             }
         }
 
         fetchPatients();
     }, []);
 
-    // Validate the form
-    const validateForm = () => {
+    /**
+     * Submit function
+     * @desc Validates the form and creates the album
+     * @function submit
+     * @returns {void}
+     */
+    const submit = (): void => {
+        setAlbumNameError(false);
+        setDescriptionError(false);
+        // Validations
         if (albumName === "") {
-            setAlbumNameError("Album name is required");
+            setAlbumNameError(true);
+            albumNameRef.current?.shake();
         }
         if (description === "") {
-            setDescriptionError("Description is required");
+            setDescriptionError(true);
+            descriptionRef.current?.shake();
         }
-        if (patient === "") {
-            setPatientNameError("Patient is required");
-        }
+        // We return here, so both errors are shown```
+        if (albumName === "" || description === "") return;
 
-        setTimeout(() => {
-            setAlbumNameError("");
-            setDescriptionError("");
-            setPatientNameError("");
-        }, 5000);
+        if (!user) return;
 
-        return !(albumName === "" || description === "" || patient === "");
-
-    }
-
-    // Submit the form
-    const submit = () => {
-        if (albumName === "" || description === "" || patient === "") {
-            console.error("Form not filled");
-            validateForm();
-            return;
-        }
-        if (!user) {
-            console.error("User not found");
-            return;
-        }
-        let data = {
+        let album = {
             title: albumName,
             description: description,
             patient: patient,
             user: user.pk
         }
 
-        console.debug("Creating album with data: ", data);
+        // Creating the album
+        console.debug("[NewAlbumForm] Submitting album: ", album);
 
-        newAlbum(data).then(
-            (response) => {
-                if (response) {
-                    onSubmitted();
-                }
+        newAlbum(album).then(() => {
+            onSubmitted();
+        });
+    }
+
+    const onChangeText = (s: string, type: "name" | "desc") => {
+        if (type === "name") {
+            if (s === "") {
+                setAlbumNameError(true);
+            } else {
+                setAlbumNameError(false);
             }
-        )
+            setAlbumName(s);
+        } else {
+            if (s === "") {
+                setDescriptionError(true);
+            } else {
+                setDescriptionError(false);
+            }
+            setDescription(s);
+        }
     }
 
     return (
@@ -125,31 +146,37 @@ export const NewAlbum = (props: NewAlbumProps) => {
                         Create a new album to sort pictures together.
                     </Text>
 
-                    <Text className={"mb-2 mt-4 dark:text-white font-bold  sm:text-sm md:text-base lg:text-lg"}>Album
-                        Name</Text>
-                    <TextInput value={albumName} onChangeText={
-                        (text) => setAlbumName(text)} key={"album_name"} placeholder={"Holidays..."}
-                               placeholderTextColor={"#AAAAA5"}
-                               className={"rounded-md p-4 border dark:text-white dark:border-gray-500 border-gray-400 focus:border-blue-500 transition-all ease-linear input"}/>
-                    <Text className={"text-red-500 mt-1"}>{albumNameError}</Text>
-
-                    <Text
-                        className={"mb-2 mt-4 dark:text-white font-bold  sm:text-sm md:text-base lg:text-lg"}>Patient</Text>
-                    <Dropdown options={patients}
-                              onSelect={
-                                  (value) => setPatient(value)} key={"patient"}
+                    {/* Album Name Input Group */}
+                    <InputGroup
+                        label={"Album Name"}
+                        placeholder={"Holidays..."}
+                        errorMessage={"This field is required"}
+                        error={albumNameError}
+                        value={albumName}
+                        ref={albumNameRef}
+                        onChangeText={(text) => onChangeText(text, "name")}
                     />
-                    <Text className={"text-red-500 mt-1"}>{patientNameError}</Text>
 
+                    {/* Patient Dropdown */}
                     <Text
-                        className={"mb-2 mt-4 dark:text-white font-bold  sm:text-sm md:text-base lg:text-lg"}>Description</Text>
-                    <TextInput value={description} onChangeText={
-                        (text) => setDescription(text)} scrollEnabled={true} key={"description"}
-                               placeholder={"A collection of holiday pictures..."}
-                               placeholderTextColor={"#AAAAA5"}
-                               className={"rounded-md p-4 border dark:text-white dark:border-gray-500 border-gray-400 focus:border-blue-500 transition-all ease-linear input"}/>
-                    <Text className={"text-red-500 mt-1"}>{descriptionError}</Text>
+                        className={"dark:text-white font-bold  sm:text-sm md:text-base lg:text-lg"}>Patient</Text>
+                    <Dropdown options={patients}
+                              onSelect={(value) => setPatient(value)}
+                              key={"patient"}
+                    />
 
+                    {/* Description Input Group */}
+                    <InputGroup
+                        label={"Description"}
+                        placeholder={"Description..."}
+                        errorMessage={"This field is required"}
+                        error={descriptionError}
+                        value={description}
+                        ref={descriptionRef}
+                        onChangeText={(text) => onChangeText(text, "desc")}
+                    />
+
+                    {/* Buttons */}
                     <View className={"flex-row justify-center gap-2"}>
                         <TouchableOpacity
                             onPress={() => {
@@ -161,9 +188,15 @@ export const NewAlbum = (props: NewAlbumProps) => {
                                 Create Album
                             </Text>
                         </TouchableOpacity>
-
                         <TouchableOpacity
-                            onPress={props.onClose}
+                            onPress={
+                                // Reset errors and close the modal
+                                () => {
+                                    setAlbumNameError(false);
+                                    setDescriptionError(false);
+                                    props.onClose();
+                                }
+                            }
                             className={"flex-row items-center w-1/2 mt-3 rounded-lg p-2 bg-red-500"}>
                             <MaterialIcons name="cancel" size={24} color="white" className={"mr-1"}/>
                             <Text className="text-white">
@@ -189,3 +222,5 @@ const styles = StyleSheet.create({
         elevation: 5,
     },
 });
+
+export default NewAlbum;
