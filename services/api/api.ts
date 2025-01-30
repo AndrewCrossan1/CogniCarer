4cff4a33bdc02ca2567af3f1cd2500492529259d
AndrewCrossan1 <andrew.crossan23@outlook.com>
2025-01-30 22:23:47 +0000

    Added method for uploading images

--- services/api/api.ts
+++ services/api/api.ts
@@ -1,6 +1,7 @@
 import axios, {AxiosInstance, AxiosResponse, InternalAxiosRequestConfig} from 'axios';
 import {ApiError, Quote} from "@/services/api/types";
 import {store} from "@/services/store/store";
+import {ImagePickerResult} from "expo-image-picker";
 
 // Define the API class
 export class API {
@@ -127,6 +128,42 @@ export class API {
     public async quote(): Promise<Quote> {
         return await this.client.get('https://zenquotes.io/api/today').then((response) =>
             response.data[0]);
+    }
+
+    public async image_post(endpoint: string, data: any, file: ImagePickerResult): Promise<any> {
+        const formData = new FormData();
+
+        if (!file.assets) {
+            return Promise.reject("No file provided");
+        }
+
+        // Get the file type
+        const uri = file.assets[0].mimeType;
+        if (!uri) {
+            return Promise.reject("Invalid file type");
+        }
+
+        // Create a new file from the image
+        const newFile = {
+            uri: file.assets[0].uri,
+            name: `photo.${uri.split('/')[1]}`,
+            type: file.assets[0].mimeType,
+        };
+
+        // Append the file to the form data
+        formData.append('image', newFile as any);
+
+        // Append the data to the form data
+        Object.keys(data).forEach((key) => {
+            formData.append(key, data[key]);
+        });
+
+        const response = await this.client.post(endpoint, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+        });
+        return response.data;
     }
 }
 
