a3a7a700ae9d24fc0f4fc59c4eb9857b09af1b2e
AndrewCrossan1 <andrew.crossan23@outlook.com>
2025-01-30 17:50:54 +0000

    Added selector and form

--- app/(app)/(reminisce)/(tabs)/Pictures.tsx
+++ app/(app)/(reminisce)/(tabs)/Pictures.tsx
@@ -13,12 +13,13 @@ import {MaterialIcons} from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
 import {Alert} from "@/components/Alert";
 import {useCallback, useEffect, useState} from "react";
-import Animated, {Easing, useAnimatedStyle, useSharedValue, withTiming} from "react-native-reanimated";
 import * as Haptics from "expo-haptics";
-import {Picture, UserAlbum} from "@/services/api/types";
+import {Picture} from "@/services/api/types";
 import {useReminisce} from "@/hooks/useReminisce";
 import {BlurView} from "expo-blur";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Selector from "@/components/forms/Selector";
+import NewPictureForm from "@/components/reminisce/NewPictureForm";
 
 const Pictures = () => {
 
@@ -33,6 +34,7 @@ const Pictures = () => {
     const [canSelect, setCanSelect] = useState(false);
     const [confirmVisible, setConfirmVisible] = useState(false);
     const [alertType, setAlertType] = useState<"success" | "error">("error");
+    const [newPictureVisible, setNewPictureVisible] = useState(false);
 
     const onSelectPress = () => {
         if (!canSelect) {
@@ -82,28 +84,6 @@ const Pictures = () => {
             setPictures(valid);
         });
     }, [getPictures]);
-
-    // Selected Animations
-    const maxHeight = useSharedValue(0)
-
-    useEffect(() => {
-        if (selected.length > 0) {
-            maxHeight.value = withTiming(200, {
-                duration: 1500,
-                easing: Easing.out(Easing.exp)
-            })
-        } else {
-            maxHeight.value = withTiming(0, {
-                duration: 1500,
-                easing: Easing.out(Easing.exp)
-            })
-        }
-    }, [selected.length]);
-
-    const animatedStyle = useAnimatedStyle(() => ({
-        maxHeight: maxHeight.value,
-        opacity: maxHeight.value > 0 ? 1 : 0
-    }))
 
     const deletePicture = useCallback(() => {
         if (selected.length === 0) {
@@ -159,6 +139,9 @@ const Pictures = () => {
                     </View>
                     <View className={"flex-row items-center justify-center gap-5 w-full"}>
                         <TouchableOpacity
+                            onPress={() => {
+                                setNewPictureVisible(!newPictureVisible);
+                            }}
                             className="flex-row items-center mt-3 w-1/2 rounded-lg p-2 bg-blue-600 dark:bg-neutral-900 drop-shadow-md shadow-blue-500/50">
                             <MaterialIcons name="upload" size={24} color="white" className={"mr-1"} />
                             <Text className="text-white">
@@ -177,36 +160,9 @@ const Pictures = () => {
                         </TouchableOpacity>
                     </View>
                 </View>
-                {/* Selected album dropdown choices (Delete, etc) */}
-                <Animated.View style={[styles.selectedContainer, animatedStyle]}>
-                    {canSelect && selected.length > 0 &&
-                      <View className={"w-full px-4 pt-4 flex-row items-center justify-between"}>
-                        <Text className={"dark:text-white text-lg font-bold"}>
-                          Selected Albums
-                        </Text>
-                        <Text className={"text-blue-500 text-sm"}>
-                            {selected.length} Selected
-                        </Text>
-                      </View>
-                    }
-                    {canSelect && selected.length > 0 &&
-                      <View>
-                        <View className={"flex-row items-center justify-center gap-5 px-4 w-full"}>
-                          <TouchableOpacity
-                            onPress={() => {
-                                setConfirmVisible(!confirmVisible);
 
-                            }}
-                            className="flex-row items-center mt-3 w-full rounded-lg p-2 bg-red-600 dark:bg-neutral-900 drop-shadow-md shadow-blue-500/50">
-                            <MaterialIcons name="delete" size={24} color="white" className={"mr-1"} />
-                            <Text className="text-white">
-                              Delete
-                            </Text>
-                          </TouchableOpacity>
-                        </View>
-                      </View>
-                    }
-                </Animated.View>
+                {/* Selected album dropdown choices (Delete, etc) */}
+                <Selector canSelect={canSelect} selected={selected} onSelect={onSelectPress} setConfirmVisible={setConfirmVisible} confirmVisible={confirmVisible} label={"Pictures"}/>
 
                 {/* Every 2 albums (Columns) create a new row */}
                 {!loading &&
@@ -323,6 +279,7 @@ const Pictures = () => {
                     </View>
                 </View>
             </Modal>
+            <NewPictureForm visible={newPictureVisible} onSubmitted={() => {}} onClose={() => setNewPictureVisible(!newPictureVisible)}/>
         </ScrollView>
     )
 }
