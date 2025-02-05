40104e8261749427ac24c44eb7ff2d7de0003bad
AndrewCrossan1 <andrew.crossan23@outlook.com>
2025-02-05 19:16:16 +0000

    Added biometric authentication and Alert Component

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -6,13 +6,17 @@ import {
     StyleSheet,
     ScrollView
 } from "react-native";
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {useRouter} from "expo-router";
 import colors from "tailwindcss/colors";
 import {useAuth} from "@/context/AuthContext";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import * as Haptics from "expo-haptics";
 import InputGroup, {InputGroupRef} from "@/components/forms/InputGroup";
+import * as SecureStore from "expo-secure-store";
+import * as LocalAuthentication from "expo-local-authentication";
+import {MaterialIcons} from "@expo/vector-icons";
+import {Alert} from "@/components/Alert";
 
 export default function LoginScreen() {
     const router = useRouter();
@@ -21,11 +25,24 @@ export default function LoginScreen() {
     const [showAlert, setShowAlert] = useState(false);
     const [passwordErrVisible, setPasswordErrVisible] = useState(false);
     const [emailErrVisible, setEmailErrVisible] = useState(false);
+    const [message, setMessage] = useState("");
+    const [alertType, setAlertType] = useState<"error" | "success">("error");
 
     const refEmailInput = useRef<InputGroupRef>(null);
     const refPasswordInput = useRef<InputGroupRef>(null);
 
+    const [checked, setChecked] = useState(false);
+
     const {login, loading} = useAuth();
+
+    // Timer to automatically close the alert after 3 seconds
+    useEffect(() => {
+        if (showAlert) {
+            setTimeout(() => {
+                setShowAlert(false);
+            }, 3000);
+        }
+    }, [showAlert]);
 
     const focusOnPassword = () => {
         if (refPasswordInput && refPasswordInput.current) {
@@ -33,6 +50,71 @@ export default function LoginScreen() {
             refPasswordInput.current.focus();
         }
     };
+
+    // Check if the user has biometric authentication enabled
+    const hasBiometrics = async () => {
+        // Check if the device has biometric hardware
+        const hasHardware = await LocalAuthentication.hasHardwareAsync();
+        if (!hasHardware) {
+            console.debug("Device does not have biometric hardware");
+            return false;
+        }
+
+        // Check if the user has biometric records
+        const hasRecords = await LocalAuthentication.isEnrolledAsync();
+        if (!hasRecords) {
+            console.debug("User does not have biometric records");
+            return false;
+        }
+        return true;
+    }
+
+    const useLocalAuth = async () => {
+        // Check if the user has biometric authentication enabled
+        if (!await hasBiometrics()) {
+            console.debug("Biometric authentication is not enabled, defaulting to login screen");
+            return;
+        }
+
+        // Check SecureStore for stored credentials
+        const email = await SecureStore.getItemAsync("email");
+        const password = await SecureStore.getItemAsync("password");
+
+        if (!email || !password) {
+            setShowAlert(true);
+            setMessage("No stored credentials found");
+            setAlertType("error");
+            console.debug("No stored credentials found, defaulting to login screen");
+            return;
+        }
+
+        // Prompt the user to authenticate with biometrics
+        const result = await LocalAuthentication.authenticateAsync({
+            promptMessage: "Please authenticate to continue",
+            cancelLabel: "Cancel"
+        });
+
+        // If the user cancels the biometric prompt, return
+        if (!result.success) {
+            console.debug("Biometric prompt cancelled, defaulting to login screen");
+            return;
+        }
+
+        // Attempt to log in with the stored credentials
+        const response = await login(email, password);
+        if (!response) {
+            setShowAlert(true);
+            Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+            setMessage("Invalid email or password");
+            setAlertType("error");
+            console.debug("Stored credentials are invalid, defaulting to login screen");
+            return;
+        }
+
+        // If the login is successful, navigate to the app
+        Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+        router.push("/(app)");
+    }
 
     const handleLogin = async () => {
         // Reset the error messages
@@ -56,9 +138,18 @@ export default function LoginScreen() {
             const response = await login(email, password);
             if (!response) {
                 setShowAlert(true);
+                setMessage("Invalid email or password");
+                setAlertType("error");
                 Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
                 return;
             }
+
+            // If the user has checked the remember me box, store the credentials
+            if (checked) {
+                await SecureStore.setItemAsync("email", email);
+                await SecureStore.setItemAsync("password", password);
+            }
+
             router.push("/(app)");
         }
     };
@@ -74,6 +165,12 @@ export default function LoginScreen() {
 
     return (
         <ScrollView className={"flex-1 w-full dark:bg-neutral-900 bg-neutral-100"}>
+            <Alert
+                message={message}
+                type={alertType}
+                visible={showAlert}
+                onPress={() => setShowAlert(false)}
+            />
             {/* Header */}
             <View className={"xs:mt-1 sm:mt-2 md:mt-4 lg:mt-6"}>
                 <Text
@@ -83,19 +180,6 @@ export default function LoginScreen() {
             </View>
             {/* Form */}
             <View className={"xs:pb-2 sm:pb-3 md:pb-4 lg:pb-5 px-8"}>
-                <View
-                    className={`rounded-md bg-red-200 flex-row justify-between p-2 items-center ${showAlert ? 'visible' : 'invisible'} xs:mb-1 sm:mb-2 md:mb-4 lg:mb-6 xs:mt-1 sm:mt-1 md:mt-4 lg:mt-6`}>
-                    <View className={"flex-row justify-center items-center"}>
-                        <FontAwesome name={"exclamation-circle"} size={20} className={"mr-2"} color={colors.red[500]}/>
-                        <Text style={styles.alertError}
-                              className={`text-center xs:text-sm sm:text-base md:text-lg lg:text-xl`}>
-                            Your email or password is incorrect
-                        </Text>
-                    </View>
-                    <FontAwesome name={"close"} size={20} className={"ml-4"} color={colors.red[500]}
-                                 onPress={() => setShowAlert(!showAlert)}/>
-                </View>
-
                 <InputGroup ref={refEmailInput} onSubmitEditing={focusOnPassword} error={emailErrVisible}
                             errorMessage={"This field is required!"} label={"Email Address"} value={email}
                             onChangeText={(e) => setEmail(e)} placeholder={"joe.bloggs@cognicarer.com"}
@@ -107,10 +191,23 @@ export default function LoginScreen() {
                             autoComplete={"password"}
                             textContentType={"password"}
                             placeholder={"Password"} secureTextEntry={true}/>
-
                 <TouchableOpacity onPress={() => router.push("/(auth)/forgot-password")}>
                     <Text className={"underline dark:text-white ml-2"}>Forgot your password?</Text>
                 </TouchableOpacity>
+
+                {/* Remember Me */}
+                <View className={"flex-row items-center ml-2"} style={{marginTop: 20}}>
+                    <TouchableOpacity onPress={() => setChecked(!checked)} className={"flex-row items-center"}>
+                        <View
+                            className={`w-6 h-6 border-2 mr-2 rounded-md border-blue-500 flex-row justify-center items-center ${checked ? 'bg-blue-500' : 'bg-transparent'}`}>
+
+                            {checked &&
+                                <FontAwesome name={"check"} size={16} color={colors.white}/>
+                            }
+                        </View>
+                        <Text className={"dark:text-white"}>Remember me</Text>
+                    </TouchableOpacity>
+                </View>
 
                 {/* Login Button */}
                 {loading ?
@@ -122,6 +219,15 @@ export default function LoginScreen() {
                         </Text>
                     </TouchableOpacity>
                 }
+
+                {/* Biometric Authentication */}
+                <TouchableOpacity onPress={useLocalAuth}
+                                  className={"w-full bg-blue-500 flex-row justify-center text-white p-2.5 rounded-md xs:mt-3 sm:mt-4 md:mt-5 lg:mt-6"}>
+                    <MaterialIcons name={"fingerprint"} size={24} color={colors.white}/>
+                    <Text className={"text-center text-white text-lg"}>
+                        Use Biometrics
+                    </Text>
+                </TouchableOpacity>
 
                 <View className={"xs:mt-1 sm:mt-3 md:mt-5 lg:mt-7 flex-row items-center"}>
                     <Text className={"text-xl dark:text-white font-bold"}>
