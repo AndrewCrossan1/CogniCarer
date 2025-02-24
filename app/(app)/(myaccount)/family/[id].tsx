bf85d26f027bfb7da9fb5fcdbcab30ccc7cac41f
AndrewCrossan1 <andrew.crossan23@outlook.com>
2025-02-24 10:50:23 +0000

    Created base file for family member view

---
+++ app/(app)/(myaccount)/family/[id].tsx
@@ -0,0 +1,11 @@
+import {Text, View} from "react-native";
+
+const familyMember = () => {
+    return (
+        <View className={"flex items-center justify-center"}>
+            <Text>Specific Family Member</Text>
+        </View>
+    );
+}
+
+export default familyMember;
