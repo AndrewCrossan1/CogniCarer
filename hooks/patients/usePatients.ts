aa937f3ef1309fd343b2aeb0aff5b767beabd3a2
AndrewCrossan1 <andrew.crossan23@outlook.com>
2025-02-25 17:16:36 +0000

    Added updatePatient and createPatient

--- hooks/patients/usePatients.ts
+++ hooks/patients/usePatients.ts
@@ -37,5 +37,58 @@ export const usePatients = () => {
         return response;
     }
 
-    return {loading, patients, error, getPatients, getPatient};
+    interface UpdatePatientData {
+        first_name: string;
+        middle_name: string;
+        last_name: string;
+        date_of_birth: string;
+        relationship: string;
+        gender: string;
+    }
+
+    const updatePatient = async (id: string,  data: UpdatePatientData, image?: any): Promise<boolean> => {
+        setLoading(true);
+        let response;
+        console.log(data);
+        if (image) {
+            // Update patient with image
+            response = null;
+        } else {
+            // Update patient without
+            response = await API.put(`patients/${id}/`, data);
+            console.log(response);
+        }
+
+        if (!response) {
+            setError("An error occurred while updating patient");
+            setLoading(false);
+            return false;
+        }
+
+        setLoading(false);
+        return true;
+    }
+
+    const createPatient = async (data: UpdatePatientData, image?: any): Promise<boolean> => {
+        setLoading(true);
+        let response;
+        if (image) {
+            // Update patient with image
+            response = await API.image_post(`patients/`, data, image, "image");
+        } else {
+            // Update patient without
+            response = await API.POST(`patients/`, data);
+        }
+
+        if (!response) {
+            setError("An error occurred while updating patient");
+            setLoading(false);
+            return false;
+        }
+
+        setLoading(false);
+        return true;
+    }
+
+    return {loading, patients, error, getPatients, getPatient, updatePatient, createPatient};
 }
