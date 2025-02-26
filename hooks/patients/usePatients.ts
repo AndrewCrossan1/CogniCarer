bc90bd8b7982d1536073ecbf2ce66216eeeae93a
AndrewCrossan1 <andrew.crossan23@outlook.com>
2025-02-26 17:22:24 +0000

    Added delete method and fixed issue with paramName in create

--- hooks/patients/usePatients.ts
+++ hooks/patients/usePatients.ts
@@ -49,14 +49,12 @@ export const usePatients = () => {
     const updatePatient = async (id: string,  data: UpdatePatientData, image?: any): Promise<boolean> => {
         setLoading(true);
         let response;
-        console.log(data);
         if (image) {
             // Update patient with image
-            response = null;
+            response = await API.image_put(`patients/${id}/`, data, image, "profile_picture");
         } else {
             // Update patient without
             response = await API.put(`patients/${id}/`, data);
-            console.log(response);
         }
 
         if (!response) {
@@ -73,10 +71,8 @@ export const usePatients = () => {
         setLoading(true);
         let response;
         if (image) {
-            // Update patient with image
-            response = await API.image_post(`patients/`, data, image, "image");
+            response = await API.image_post(`patients/`, data, image, "profile_picture");
         } else {
-            // Update patient without
             response = await API.POST(`patients/`, data);
         }
 
@@ -90,5 +86,21 @@ export const usePatients = () => {
         return true;
     }
 
-    return {loading, patients, error, getPatients, getPatient, updatePatient, createPatient};
+    const deletePatient = async (id: string): Promise<boolean> => {
+        setLoading(true);
+
+        const response = await API.delete(`patients/${id}/`);
+
+        // If there is a response, then there was an error, because the response is 204 (no content) on success
+        if (response) {
+            setError("An error occurred while deleting patient");
+            setLoading(false);
+            return false;
+        }
+
+        setLoading(false);
+        return true;
+    }
+
+    return {loading, patients, error, getPatients, getPatient, updatePatient, createPatient, deletePatient};
 }
