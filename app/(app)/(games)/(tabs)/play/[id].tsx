d263a606b7e0590970d85d50f82a5cbd4e8d1cf1
AndrewCrossan1 <andrew.crossan23@outlook.com>
2025-03-14 13:15:13 +0000

    Partial Feature:
    Implemented pre-play game view to show game information and previous scores.

---
+++ app/(app)/(games)/(tabs)/play/[id].tsx
@@ -0,0 +1,202 @@
+import {ActivityIndicator, Text, TouchableOpacity, View} from "react-native";
+import {useLocalSearchParams} from "expo-router";
+import React, {useEffect, useState} from "react";
+import {useGame} from "@/hooks/games/useGame";
+import {Match} from "@/services/api/types";
+import {useColorScheme} from "nativewind";
+import colors from "tailwindcss/colors";
+import {Dropdown} from "@/components/forms/Dropdown";
+import {usePatients} from "@/hooks/patients/usePatients";
+import {useGameStatistics} from "@/hooks/games/useGameStatistics";
+
+const SelectedGameHome = () => {
+    const { getGame, loading } = useGame();
+    const { getAttemptsByGameAndPatient } = useGameStatistics();
+    const { id } = useLocalSearchParams();
+    const [game, setGame] = useState<Match | null>(null);
+    const { colorScheme } = useColorScheme();
+    const { getPatients } = usePatients();
+    const [selectedPatient, setSelectedPatient] = useState(null);
+    const [patientOptions, setPatientOptions] = useState([] as { value: string, display: string }[]);
+
+
+    const fetchGame = async () => {
+        if (id) {
+            let tempId = (id).toString();
+            getGame(tempId).then((game) => {
+                if (game) {
+                    setGame(game);
+                }
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
+
+        getPatients().then((patients) => {
+            let tempOptions = [] as { value: string, display: string }[];
+            if (patients) {
+                patients.forEach((patient) => {
+                    tempOptions.push({
+                        value: patient.uuid,
+                        display: patient.first_name + " " + patient.last_name
+                    });
+                });
+                setPatientOptions(tempOptions);
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+
+    useEffect(() => {
+        fetchGame();
+    }, [id]);
+
+    useEffect(() => {
+        console.log("Patient", selectedPatient);
+        console.log("Game", id);
+        if (selectedPatient) {
+            getAttemptsByGameAndPatient(selectedPatient, id.toString()).then((attempts) => {
+                console.log(attempts);
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
+    }, [selectedPatient]);
+
+    return (
+        <View className={"flex-1 justify-between items-center bg-neutral-100 dark:bg-neutral-800 md:px-4 lg:px-6"}>
+            {loading && (
+                <ActivityIndicator size={"large"} color={colors.blue[500]} />
+            )}
+
+            {!loading && game && (
+                <View className={"w-full h-full justify-between"}>
+                    <View className={"py-16"}>
+                        <View className={"items-center my-4"}>
+                            <Text className="text-4xl font-bold text-neutral-900 dark:text-white">
+                                {game.title}
+                            </Text>
+                            <Text className="text-lg text-neutral-500 dark:text-neutral-400">
+                                By: {game.person_with_dementia || "The CogniCarer Team"}
+                            </Text>
+                        </View>
+
+                        <View className={"flex flex-row gap-3 my-4"}>
+                            <View
+                                className="flex-1 py-4 bg-white dark:bg-neutral-900 rounded-lg items-center" style={{
+                                shadowColor: colors.black,
+                                shadowOffset: {width: 0, height: 2},
+                                shadowOpacity: colorScheme === "dark" ? 0.30 : 0.10,
+                                shadowRadius: 3.84,
+                                elevation: 2
+                            }}>
+                                <Text className="text-4xl font-bold text-blue-500">
+                                    {game.average_score}
+                                </Text>
+                                <Text className="text-sm text-neutral-500 dark:text-neutral-400">
+                                    Average Score
+                                </Text>
+                            </View>
+
+                            <View
+                                className="flex-1 py-4 bg-white dark:bg-neutral-900 rounded-lg items-center" style={{
+                                shadowColor: colors.black,
+                                shadowOffset: {width: 0, height: 2},
+                                shadowOpacity: colorScheme === "dark" ? 0.30 : 0.10,
+                                shadowRadius: 3.84,
+                                elevation: 2
+                            }}>
+                                <Text className="text-4xl font-bold text-blue-500">
+                                    {game.maximum_score}
+                                </Text>
+                                <Text className="text-sm text-neutral-500 dark:text-neutral-400">
+                                    Maximum Score
+                                </Text>
+                            </View>
+
+                            <View
+                                className="flex-1 py-4 bg-white dark:bg-neutral-900 rounded-lg items-center" style={{
+                                shadowColor: colors.black,
+                                shadowOffset: {width: 0, height: 2},
+                                shadowOpacity: colorScheme === "dark" ? 0.30 : 0.10,
+                                shadowRadius: 3.84,
+                                elevation: 2
+                            }}>
+                                <Text className="text-4xl font-bold text-blue-500">
+                                    {game.times_played}
+                                </Text>
+                                <Text className="text-sm text-neutral-500 dark:text-neutral-400">
+                                    Total Play{game.times_played > 1 ? "s" : ""}
+                                </Text>
+                            </View>
+                        </View>
+
+                        <View className={"flex items-center my-2"}>
+                            {/* Description */}
+                            <Text className="text-lg dark:text-white" numberOfLines={2}>
+                                {game.description || "No description available"}
+                            </Text>
+                        </View>
+
+                        <View className={"flex my-2 items-center"}>
+                            <View className={"flex flex-col my-2 items-start"}>
+                                <Text className={"text-lg tracking-wide font-semibold dark:text-white text-start"}>
+                                    Round 1: {game.round_1_time} seconds
+                                </Text>
+
+                                <Text className={"text-lg tracking-wide font-semibold dark:text-white text-start"}>
+                                    Round 2: {game.round_2_time} seconds
+                                </Text>
+
+                                <Text className={"text-lg tracking-wide font-semibold dark:text-white text-start"}>
+                                    Round 3: {game.round_3_time} seconds
+                                </Text>
+                            </View>
+                        </View>
+
+                        <View className={"flex my-2"}>
+                            <Text className={"text-lg dark:text-white font-semibold my-2"}>
+                                Select the player
+                            </Text>
+                            <Dropdown options={patientOptions} onSelect={(value => {
+                                setSelectedPatient(value);
+                            })} />
+                        </View>
+
+                        <Text className={"text-lg dark:text-white font-semibold mt-2"}>
+                            Previous Attempts
+                        </Text>
+                        <View className={"w-full flex border-b dark:border-b-neutral-600 border-b-neutral-300"}/>
+
+                        <View className={"flex flex-row gap-3 mt-3"}>
+                            <View className={"flex-1 py-4 bg-white dark:bg-neutral-900 rounded-lg items-center"}>
+                                <Text className={"text-lg dark:text-white"}>
+                                    1. 10 points
+                                </Text>
+                            </View>
+                            <View className={"flex-1 py-4 bg-white dark:bg-neutral-900 rounded-lg items-center"}>
+                                <Text className={"text-lg dark:text-white"}>
+                                    1. 10 points
+                                </Text>
+                            </View>
+                            <View className={"flex-1 py-4 bg-white dark:bg-neutral-900 rounded-lg items-center"}>
+                                <Text className={"text-lg dark:text-white"}>
+                                    1. 10 points
+                                </Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <TouchableOpacity className={"flex bg-blue-500 text-white p-4 px-4 rounded-lg my-4"}>
+                        <Text className={"text-lg font-semibold text-white text-center"}>
+                            Play Game
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            )}
+        </View>
+    );
+}
+
+export default SelectedGameHome;
