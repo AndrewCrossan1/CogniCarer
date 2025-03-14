94f3fb83bc8ab97320f167b6b5055f4df759ff97
AndrewCrossan1 <andrew.crossan23@outlook.com>
2025-03-14 13:17:06 +0000

    Partial Feature:
    Added basic screen for playing games.

---
+++ app/(app)/(games)/(tabs)/play/play.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from "react-native";
+
+const PlayGame = () => {
+    return (
+        <View>
+            <Text>Play Game</Text>
+        </View>
+    );
+}
+
+export default PlayGame;
